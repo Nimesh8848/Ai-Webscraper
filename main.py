3c4c33cd2f5d87ff31f323c46219159d1947e69a
Nimesh8848 <nimeshmathema@gmail.com>
2025-01-26 20:43:52 +0545

    Project completed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from scrape import (
     clean_body_content,
     split_dom_content,
 )
+from parse import parse_with_ollama
 st.title('AI WebScraper')
 url = st.text_input('Enter Website URL')
 
@@ -25,3 +26,16 @@ if st.button("Scrape Website"):
         # Display the DOM content in an expandable text box
         with st.expander("View DOM Content"):
             st.text_area("DOM Content", cleaned_content, height=300)
+
+if "dom_content" in st.session_state:
+    parse_desc=st.text_area('Enter Description')
+
+    if st.button("Parse Content"):
+        if parse_desc:
+            st.write("parsing...")
+
+            # Split the DOM content into chunks
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks, parse_desc)
+            st.write(result)
+           
